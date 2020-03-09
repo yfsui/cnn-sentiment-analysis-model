f845bb77577161d6f1465ce9ac7b3a4f2f7039cf
Yufeng Sui <44793462+yfsui@users.noreply.github.com>
2020-03-09 17:06:33 -0400

    update function to save model

--- model_training/sentiment_model_cnn.py
+++ model_training/sentiment_model_cnn.py
@@ -30,10 +30,13 @@ def keras_model_fn(_, config):
     return cnn_model
 
 def save_model(model, output):
-    """
-    Method to save models in SaveModel format with signature to allow for serving
 
     """
-    model.save(output)
+    Method to save a model in SaveModel format with signature to allow for serving
+    """
+
+    print("Saving model...")
+
+    tf.saved_model.save(model, os.path.join(output, "1"))
 
     print("Model successfully saved at: {}".format(output))
